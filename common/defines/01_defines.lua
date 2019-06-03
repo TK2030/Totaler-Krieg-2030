0bd28f416c20d2b585a865c0e3e8f71f19f5aaf0
CapalVictorous <capalvictorous@gmail.com>
2019-06-02 23:15:37 -0600

    changed some stuff
    woah, right

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -1,5 +1,6 @@
-NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 48			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 240			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1				-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 10					-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 20					-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 24000000
 NDefines.NGame.END_DATE = "2042.1.1.1";
